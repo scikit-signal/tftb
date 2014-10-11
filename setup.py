f63f49c38eba9871010b97eb3e8c1cad32c3d552
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2014-10-11 10:57:12 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+NAME = "tftb"
+
+setup(
+    name=NAME,
+    version='0.0.1',
+    author='Jaidev Deshpande',
+    author_email='deshpande.jaidev@gmail.com',
+    packages=['tftb',
+              'tftb.processing',
+              'tftb.visualization',
+              'tftb.generators',
+              'tftb.postprocessing']
+)
