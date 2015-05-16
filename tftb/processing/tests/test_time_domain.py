9043fa5d6fd8ea77fc3267f94348a9265e81b290
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2015-05-16 18:05:57 +0530

    TEST: Add tests for time localization characteristics

---
+++ tftb/processing/tests/test_time_domain.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2015 jaidev <jaidev@newton>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+
+import unittest
+from tftb.processing import time_domain as tmd
+from tftb.generators import amplitude_modulated as am
+from tftb.tests.base import TestBase
+
+
+class TestTimeDomainProcessors(TestBase):
+
+    def test_loctime(self):
+        signal = am.amgauss(160, 80, 50)
+        tm, T = tmd.loctime(signal)
+        self.assertAlmostEqual(tm, 79, places=6)
+        self.assertAlmostEqual(T, 50, places=4)
+
+if __name__ == '__main__':
+    unittest.main()
