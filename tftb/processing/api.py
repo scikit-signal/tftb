081a1c49b6477938955b99291833af88edde9fba
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2015-05-25 11:01:34 +0530

    Add example plotting instantaneous frequencies

--- tftb/processing/api.py
+++ tftb/processing/api.py
@@ -1,2 +1,3 @@
 from tftb.processing.time_domain import loctime
-from tftb.processing.freq_domain import locfreq
+from tftb.processing.freq_domain import locfreq, inst_freq
+from tftb.processing.plotifl import plotifl
