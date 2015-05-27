54d1532bedb7b889ebbd2b71e6c1d6dd8063ac5e
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2015-05-27 20:49:13 +0530

    Add Gabor representation

--- tftb/processing/linear.py
+++ tftb/processing/linear.py
@@ -11,6 +11,68 @@ Linear Time Frequency Processing.
 """
 
 import numpy as np
+from tftb.utils import nearest_odd, divider, modulo, izak
+
+
+def gabor(signal, n_coeff=None, q_oversample=None, window=None):
+    """Compute the Gabor representation of a signal.
+
+    :param signal: Singal to be analyzed.
+    :param n_coeff: number of Gabor coefficients in time.
+    :param q_oversample: Degree of oversampling
+    :param window: Synthesis window
+    :type signal: array-like
+    :type n_coeff: integer
+    :type q_oversample: int
+    :type window: array-like
+    :return: Tuple of Gabor coefficients, biorthogonal window associated with the synthesis window.
+    :rtype: tuple
+    """
+    if n_coeff is None:
+        n_coeff = divider(signal.shape[0])
+    if q_oversample is None:
+        q_oversample = divider(n_coeff)
+    if window is None:
+        window = np.exp(np.log(0.005) * np.linspace(-1, 1, nearest_odd(n_coeff)) ** 2)
+        window = window / np.linalg.norm(window)
+    m = q_oversample * signal.shape[0] / float(n_coeff)
+    mb = signal.shape[0] / float(n_coeff)
+    nb = signal.shape[0] / float(m)
+
+    # Zak transform?
+    nh = window.shape[0]
+    if nh % 2 == 0:
+        raise ValueError("The window function should have an odd length.")
+    alpha = np.round((2 * signal.shape[0] / float(n_coeff) - 1 - nh) / (2 *
+        q_oversample))
+    hn1 = np.zeros((signal.shape[0],))
+    start = np.round(((signal.shape[0] - (nh - 1))) / 2) - alpha
+    end = np.round((signal.shape[0] + nh - 1) / 2) - alpha
+    hn1[np.arange(start - 1, end).astype(int)] = window
+
+    msig = hn1.reshape(nb, m, order='F')
+    dzth = np.fft.fft(msig.T, axis=0) / np.sqrt(m)
+    mzh = np.zeros((m, mb))
+    x = np.arange(1, m + 1, dtype=float)
+    for l in xrange(q_oversample):
+        mod = modulo(x - l * m / q_oversample, m).astype(int)
+        mzh += np.abs(dzth[mod - 1, :]) ** 2
+
+    mzh[mzh < np.spacing(1)] = 1
+
+    # Za transform of biorthogonal dual frame window gam
+    dztgam = dzth / mzh
+    gam = np.real(izak(dztgam)) / signal.shape[0]
+
+    # Computation of Gabor coefficient of dual frame window.
+    dgrn1 = np.zeros((signal.shape[0], n_coeff), dtype=complex)
+    k = np.arange(1, signal.shape[0] + 1)
+    for n in xrange(n_coeff):
+        index = modulo(k - n * m / q_oversample, signal.shape[0]).astype(int) - 1
+        dgrn1[:, n] = np.fft.fft(signal * np.fft.fftshift(gam[index]), axis=0)
+    dgr = dgrn1[np.arange(signal.shape[0], step=nb).astype(int), :]
+    tfr = np.abs(dgr) ** 2
+    return tfr, dgr, gam
 
 
 def stft(signal, time_instants=None, n_fbins=None, window=None):
@@ -48,11 +110,7 @@ def stft(signal, time_instants=None, n_fbins=None, window=None):
         end = np.min([np.round(n_fbins / 2.0) - 1, lh, signal.shape[0] - ti])
         tau = np.arange(start, end + 1).astype(int)
         indices = np.remainder(n_fbins + tau, n_fbins)
-        try:
-            tfr[indices.astype(int), icol] = signal[ti + tau - 1] * np.conj(window[lh + tau])
-        except IndexError:
-            from IPython.core.debugger import Tracer
-            Tracer()()
+        tfr[indices.astype(int), icol] = signal[ti + tau - 1] * np.conj(window[lh + tau])
     tfr = np.fft.fft(tfr, axis=0)
 
     if n_fbins % 2 == 0:
@@ -63,9 +121,9 @@ def stft(signal, time_instants=None, n_fbins=None, window=None):
     return tfr, time_instants, freqs
 
 if __name__ == '__main__':
-    import matplotlib.pyplot as plt
-    from tftb.generators.api import fmconst
-    sig = np.hstack((fmconst(128, 0.2)[0], fmconst(128, 0.4)[0]))
-    tfr, t, f = stft(sig)
-    plt.imshow(np.abs(tfr))
-    plt.show()
+    from tftb.generators.api import fmlin
+    sig, _ = fmlin(128)
+    a, b, c = gabor(sig, 64, 32)
+    from matplotlib.pyplot import imshow, show
+    imshow(a)
+    show()
---
+++ tftb/utils.py
@@ -0,0 +1,81 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2015 jaidev <jaidev@newton>
+#
+# Distributed under terms of the MIT license.
+
+"""Miscellaneous utilities."""
+
+import numpy as np
+
+
+def izak(x):
+    """Inverse Zak transform."""
+    if x.ndim == 2:
+        n, m = x.shape
+    else:
+        n, m = x.shape[0], 1
+    sig = np.zeros((n * m, ), dtype=complex)
+    for im in xrange(m):
+        sig[im + np.arange(n) * m] = np.sqrt(n) * np.fft.ifft(x[:, im], axis=0)
+    return sig
+
+
+def nextpow2(n):
+    """Compute the exponent of the next higher power of 2.
+
+    :param n: Number whose next higest power of 2 needs to be computed.
+    :type n: int, np.ndarray
+    :rtype: int, np.ndarray
+    """
+    m_f = np.log2(n)
+    m_i = np.ceil(m_f)
+    return m_i
+
+
+def divider(N):
+    """Compute two factors of N such that they are as close as possible to sqrt(N).
+
+    :param N: Number to be divided.
+    """
+    n = np.floor(np.sqrt(N))
+    while True:
+        old = n
+        m = np.ceil(N / float(n))
+        n = np.floor(N / float(m))
+        if n == old:
+            break
+    return n, m
+
+
+def nearest_odd(N):
+    """Get the nearest odd number for each value of N."""
+    if isinstance(N, np.ndarray):
+        y = np.floor(N)
+        y[np.remainder(y, 2) == 0] = np.ceil(N[np.remainder(y, 2) == 0])
+        y[np.remainder(y, 2) == 0] += 1
+        return y
+    if N % 2 == 0:
+        return N + 1
+    return N
+
+
+def modulo(x, N):
+    """Compute the congruence of each element of x modulo N.
+
+    :type x: array-like
+    :type N: int
+    :return: array-like
+    """
+    if any(np.isreal(x)):
+        y = np.mod(x, N)
+        y[y == 0] = N
+    else:
+        y = np.mod(np.real(x), N) + 1j * np.mod(np.imag(x), N)
+    return y
+
+
+if __name__ == '__main__':
+    print nearest_odd(np.arange(10))
