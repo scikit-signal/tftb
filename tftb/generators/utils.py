5679371ee703ab1f4315f5a373b0e6656133540f
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2015-05-30 15:10:19 +0530

    WIP: Initial implementation of scaling functions

--- tftb/generators/utils.py
+++ tftb/generators/utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+from scipy.signal import hilbert
+# from tftb.utils import nextpow2
 
 
 def sigmerge(x1, x2, ratio=0.0):
@@ -22,3 +24,84 @@ def sigmerge(x1, x2, ratio=0.0):
     h = np.sqrt(ex1 / (ex2 * 10 ** (ratio / 10.0)))
     sig = x1 + h * x2
     return sig
+
+
+def integrate(y, x):
+    n = y.shape[0]
+    dy = y[:(n - 1)] + y[1:]
+    dx = (x[1:n] - x[:(n - 1)]) / 2.0
+    return np.dot(dy, dx)
+
+
+def scale(X, a, fmin, fmax, N):
+    """Scale a signal with the Mellin transform.
+
+    :param X: signal to be scaled.
+    :param a: scale factor
+    :param fmin: lower frequency bound
+    :param fmax: higher frequency bound
+    :param N: number of analyzed voices
+    :type X: array-like
+    :type a: float
+    :type fmin: float
+    :type fmax: float
+    :type N: int
+    :return: A-scaled version of X.
+    :rtype: array-like
+    """
+    Z = hilbert(np.real(X))
+    T = X.shape[0]
+    M = (T + np.remainder(T, 2)) / 2
+
+    # B = fmax - fmin
+    # R = B / ((fmin + fmax) / 2)
+    # Nq = np.ceil((B * T * (1 + 2.0 / R)) * np.log())
+    # Nmin = Nq - np.remainder(Nq, 2)
+    # Ndflt = 2 ** nextpow2(Nmin)
+
+    # Geometric sampling of the analyzed spectrum
+    k = np.arange(N)
+    q = (fmax / float(fmin)) ** (1.0 / (N - 1.0))
+    geo_f = fmin * np.exp((k - 1) * np.log(q))
+    t = np.arange(X.shape[0]) - M - 1
+    tfmatx = np.exp(-2 * 1j * np.dot(t.reshape(128, 1), geo_f.reshape(1, 128)) * np.pi)
+    ZS = np.dot(Z, tfmatx)
+    ZS = np.hstack((ZS, np.zeros((N,))))
+
+    # Mellin transform computation of the analyzed signal
+    p = np.arange(2 * N)
+    MS = np.fft.fftshift(np.fft.ifft(ZS, axis=0))
+    beta = (p / float(N) - 1.0) / (2 * np.log(q))
+
+    # Inverse mellin and fourier transform.
+    Mmax = np.amax(np.ceil(X.shape[0] / 2.0 * a))
+    if isinstance(a, np.ndarray):
+        S = np.zeros((2 * Mmax, a.shape[0]), dtype=complex)
+    else:
+        S = np.zeros((2 * Mmax,), dtype=complex)
+        ptr = 0
+        DMS = np.exp(-2 * np.pi * 1j * beta * np.log(a)) * MS
+        DS = np.fft.fft(np.fft.fftshift(DMS), axis=0)
+        Mcurrent = np.ceil(a * X.shape[0] / 2)
+        t = np.arange(-Mcurrent, Mcurrent) - 1
+        itfmatx = np.exp(2 * 1j * np.pi * np.dot(t.reshape((256, 1)),
+                                                 geo_f.reshape((1, 128))))
+        dilate_sig = np.zeros((2 * Mcurrent,), dtype=complex)
+        for kk in xrange(2 * int(Mcurrent)):
+            dilate_sig[kk] = integrate(itfmatx[kk, :] * DS[:N], geo_f)
+        S[(Mmax - Mcurrent):(Mmax + Mcurrent)] = dilate_sig
+        ptr += 1
+
+    S = S * np.linalg.norm(X) / np.linalg.norm(S)
+    return S
+
+if __name__ == '__main__':
+    # sig=klauder(128); S=scale(sig,2,.05,.45,128);
+    # subplot(211); plot(sig); subplot(212); plot(real(S(65:192)));
+    from tftb.generators.api import klauder
+    sig = klauder(128)
+    S = scale(sig, 2, 0.05, 0.45, 128)
+    import matplotlib.pyplot as plt
+    plt.subplot(211), plt.plot(sig)
+    plt.subplot(212), plt.plot(np.real(S[64:]))
+    plt.show()
