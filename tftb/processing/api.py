dc3dd70dcb39d1038748e863fa8e487f1c009be5
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2015-06-05 13:48:53 +0530

    Example comparing ideal TFR, spectrogram and reassigned spectrogram

--- tftb/processing/api.py
+++ tftb/processing/api.py
@@ -2,4 +2,6 @@ from tftb.processing.time_domain import loctime
 from tftb.processing.freq_domain import locfreq, inst_freq, group_delay
 from tftb.processing.plotifl import plotifl
 from tftb.processing.cohen import (wigner_ville, pseudo_wigner_ville,
-        smoothed_pseudo_wigner_ville, margenau_hill)
+        smoothed_pseudo_wigner_ville, margenau_hill, spectrogram)
+from tftb.processing.reassigned import spectrogram as reassigned_spectrogram
+from tftb.processing.misc import ideal_tfr
