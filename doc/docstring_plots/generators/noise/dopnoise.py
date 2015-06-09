b80ead149592b66e955fc47328c2f4e5f6f7a1ce
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2015-06-10 05:17:51 +0530

    Add docstring examples for tftb.generators.noise module

---
+++ doc/docstring_plots/generators/noise/dopnoise.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2015 jaidev <jaidev@newton>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from tftb.generators.api import dopnoise
+from tftb.processing.freq_domain import inst_freq
+
+z, iflaw = dopnoise(500, 200.0, 60.0, 10.0, 70.0, 128.0)
+plt.subplot(211), plt.plot(np.real(z))
+plt.grid()
+plt.title('Complex noisy Doppler signal')
+plt.xlim(0, 500)
+ifl = inst_freq(z, np.arange(11, 479), 10)
+plt.subplot(212)
+plt.plot(iflaw, 'r', label='actual')
+plt.plot(ifl, 'g', label='estimated')
+plt.xlim(0, 500)
+plt.legend()
+plt.grid()
+plt.title('Instantaneous frequency')
+plt.show()
---
+++ doc/docstring_plots/generators/noise/noisecu.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2015 jaidev <jaidev@newton>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+
+
+from tftb.generators.api import noisecu
+import numpy as np
+import matplotlib.pyplot as plt
+
+noise = noisecu(512)
+print np.real(noise.mean())
+print noise.std() ** 2
+
+plt.subplot(211), plt.plot(np.real(noise))
+plt.title('Analytic complex white noise')
+plt.xlim(0, 512)
+plt.grid()
+plt.subplot(212), plt.plot(np.linspace(-0.5, 0.5, 512),
+                           np.abs(np.fft.fftshift(np.fft.fft(noise))) ** 2)
+plt.title('Energy spectrum')
+plt.xlabel('Normalized Frequency')
+plt.xlim(-0.5, 0.5)
+plt.grid()
+plt.show()
