644a358c9375b497deeb036a327e303657d5859f
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2015-06-10 06:59:06 +0530

    Fix gallery examples for chirp signals

---
+++ doc/_gallery/1_3_1_chirp_wv.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2015 jaidev <jaidev@newton>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Example in section 1.3.1
+"""
+
+from tftb.generators.api import fmlin
+from tftb.processing.cohen import wigner_ville
+import matplotlib.pyplot as plt
+import numpy as np
+
+n_points = 128
+fmin, fmax = 0.0, 0.5
+signal, _ = fmlin(n_points, fmin, fmax)
+
+# Wigner-Ville distribution of the chirp.
+
+tfr = wigner_ville(signal)
+threshold = np.amax(tfr) * 0.05
+tfr[tfr <= threshold] = 0.0
+plt.contour(tfr, extent=[0, n_points, fmin, fmax])
+plt.grid()
+plt.title('Wigner-Ville distribution (threshold = 5%)')
+plt.xlabel('Time')
+plt.ylabel('Normalized Frequency')
+plt.show()
