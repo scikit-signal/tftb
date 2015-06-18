f1f47efb43c7a5c26899408bbb8337202d0e71c1
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2015-06-18 09:42:36 +0530

    Remove the tftb.processing.api module
    Everything is in the tftb.processing namespace

--- tftb/processing/api.py
+++
@@ -1,9 +0,0 @@
-from tftb.processing.time_domain import loctime
-from tftb.processing.freq_domain import locfreq, inst_freq, group_delay
-from tftb.processing.plotifl import plotifl
-from tftb.processing.cohen import (wigner_ville, pseudo_wigner_ville,
-        smoothed_pseudo_wigner_ville, margenau_hill, spectrogram)
-from tftb.processing.reassigned import spectrogram as reassigned_spectrogram
-from tftb.processing.reassigned import smoothed_pseudo_wigner_ville as reassigned_smoothed_pseudo_wigner_ville
-from tftb.processing.postprocessing import ideal_tfr, renyi_information
-from tftb.processing.affine import scalogram, bertrand, d_flandrin, unterberger
