ca46b506c86a9a6e4748a7cba9ef7f61e93e78c3
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2015-06-24 07:34:01 +0530

    Add class for pseudo Wigner Ville distribution

--- tftb/processing/__init__.py
+++ tftb/processing/__init__.py
@@ -1,7 +1,8 @@
 from tftb.processing.time_domain import loctime
 from tftb.processing.freq_domain import locfreq, inst_freq, group_delay
 from tftb.processing.plotifl import plotifl
-from tftb.processing.cohen import (WignerVilleDistribution, pseudo_wigner_ville,
+from tftb.processing.cohen import (WignerVilleDistribution,
+        PseudoWignerVilleDistribution,
         smoothed_pseudo_wigner_ville, MargenauHillDistribution, Spectrogram)
 from tftb.processing.reassigned import spectrogram as reassigned_spectrogram
 from tftb.processing.reassigned import smoothed_pseudo_wigner_ville as reassigned_smoothed_pseudo_wigner_ville
@@ -10,7 +11,7 @@ from tftb.processing.affine import scalogram, bertrand, d_flandrin, unterberger
 from tftb.processing.linear import ShortTimeFourierTransform
 
 __all__ = ['loctime', 'locfreq', 'inst_freq', 'group_delay', 'plotifl',
-           'WignerVilleDistribution', 'pseudo_wigner_ville',
+           'WignerVilleDistribution', 'PseudoWignerVilleDistribution',
            'smoothed_pseudo_wigner_ville', 'MargenauHillDistribution', 'Spectrogram',
            'reassigned_spectrogram', 'reassigned_smoothed_pseudo_wigner_ville',
            'ideal_tfr', 'renyi_information', 'scalogram', 'bertrand',
