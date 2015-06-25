87f31cda00d1247d7fe9946b3c9e480a4925870e
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2015-06-25 10:34:40 +0530

    Remove scipy.signal.hilbert from tftb.generators namespace

--- tftb/generators/__init__.py
+++ tftb/generators/__init__.py
@@ -4,11 +4,11 @@ from frequency_modulated import (fmconst, fmhyp, fmlin, fmodany, fmpar,
 from utils import sigmerge, scale
 from noise import dopnoise, noisecg, noisecu
 from analytic_signals import (anaask, anabpsk, anafsk, anapulse, anaqpsk,
-        anasing, anastep, hilbert)
+        anasing, anastep)
 from tftb.generators.misc import doppler, gdpower, klauder, mexhat, altes, atoms
 
 __all__ = ['amexpos', 'amgauss', 'amrect', 'amtriang', 'fmconst', 'fmhyp',
            'fmlin', 'fmodany', 'fmpar', 'fmpower', 'fmsin', 'sigmerge',
            'scale', 'dopnoise', 'noisecg', 'noisecu', 'anaask', 'anabpsk',
-           'anafsk', 'anapulse', 'anaqpsk', 'anasing', 'anastep', 'hilbert',
+           'anafsk', 'anapulse', 'anaqpsk', 'anasing', 'anastep',
            'doppler', 'gdpower', 'klauder', 'mexhat', 'altes', 'atoms']
