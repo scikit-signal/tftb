0bfa6ccced7bfee61c67090f533028e9795e427e
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2016-02-09 15:23:09 +0530

    Change dirac impulse contour plot to log scale

--- doc/_gallery/plot_4_2_3_impulse_wv.py
+++ doc/_gallery/plot_4_2_3_impulse_wv.py
@@ -20,21 +20,8 @@ Figure 4.24 from the tutorial.
 
 from tftb.generators import anapulse
 from tftb.processing import WignerVilleDistribution
-import matplotlib.pyplot as plt
-import numpy as np
 
 sig = anapulse(128)
-tfr = WignerVilleDistribution(sig).run()[0]
-t = np.arange(tfr.shape[1])
-f = np.linspace(0, 0.5, tfr.shape[0])
-
-threshold = (np.abs(tfr) ** 2) * 0.05
-tfr[(np.abs(tfr) ** 2) <= threshold] = 0.0
-
-plt.figure()
-plt.contour(t, f, np.abs(tfr) ** 2, 4)
-plt.grid(True)
-plt.title('Wigner Ville Distribution of a Dirac impulse.')
-plt.xlabel('Time')
-plt.ylabel('Normalized Frequency')
-plt.show()
+wvd = WignerVilleDistribution(sig)
+wvd.run()
+wvd.plot(kind="contour", scale="log")
