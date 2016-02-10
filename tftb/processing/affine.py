Fix Bertrand distribution numerically
Add a class for implementing bertrand distribution. At this point the output is
numerically identical to MATLAB, the plotting needs to be fixed.

tftb/processing/affine.py
@@ -17,7 +17,298 @@ from scipy.optimize import brenth, newton
 from scipy.interpolate import splrep, splev
 from tftb.generators import mexhat, scale
 from tftb.processing.utils import integrate_2d
+from tftb.processing.base import BaseTFRepresentation
 from tftb.utils import nextpow2
+
+
+class AffineDistribution(BaseTFRepresentation):
+
+    def __init__(self, signal, **kwargs):
+        super(AffineDistribution, self).__init__(signal, **kwargs)
+        if signal.ndim == 2:
+            self.kind = "cross"
+        else:
+            self.kind = "auto"
+
+    def _normalize(self):
+        pass
+
+    def _geometric_sample(self):
+        pass
+
+    def _mellin_transform(self):
+        pass
+
+
+class BertrandDistribution(AffineDistribution):
+
+    name = "Bertrand"
+
+    def __init__(self, signal, fmin=None, fmax=None, n_voices=None, **kwargs):
+        super(BertrandDistribution, self).__init__(signal, **kwargs)
+        if self.kind == "auto":
+            self.x1 = self.signal.copy()
+            self.x2 = self.signal.copy()
+        else:
+            self.x1, self.x2 = self.signal[:, 0].copy(), self.signal[:, 1].copy()
+        self.s1 = np.real(self.x1)
+        self.s2 = np.real(self.x2)
+        if (fmin is None) or (fmax is None):
+            stf1 = np.fft.fft(np.fft.fftshift(self.s1[self.ts.min():self.ts.max() + 1]))
+            stf2 = np.fft.fft(np.fft.fftshift(self.s2[self.ts.min():self.ts.max() + 1]))
+            nstf = stf1.shape[0]
+            sp1 = np.abs(stf1[:int(np.round(nstf / 2.0))]) ** 2
+            sp2 = np.abs(stf2[:int(np.round(nstf / 2.0))]) ** 2
+            maxsp1 = sp1.max()
+            maxsp2 = sp2.max()
+            f = np.linspace(0, 0.5, np.round(nstf / 2.0) + 1)
+            if fmin is None:
+                mask = sp1 > maxsp1 / 100.0
+                indmin = np.arange(mask.shape[0], dtype=int)[mask.astype(bool)].min()
+                fmin = max([0.01, 0.05 * np.floor(f[indmin] / 0.05)])
+            if fmax is None:
+                mask = sp2 > maxsp2 / 100.0
+                indmax = np.arange(mask.shape[0], dtype=int)[mask.astype(bool)].max()
+                fmax = 0.05 * np.ceil(f[indmax] / 0.05)
+        self.fmax, self.fmin = fmax, fmin
+        bw = fmax - fmin
+        R = bw / (fmin + fmax) * 2.0
+        umaxbert = lambda x: np.exp(x) - fmax / fmin
+        self.umax = brenth(umaxbert, 0, 4)
+        m = (self.signal.shape[0] + (self.signal.shape[0] % 2)) / 2
+        teq = m / (fmax * self.umax)
+        if teq < self.signal.shape[0]:
+            m0 = np.round((2 * m ** 2) / teq - m) + 1
+            m1 = m + m0
+            T = 2 * m1 - 1
+        else:
+            m0 = 0
+            m1 = m
+        self.m1 = m1
+        self.m = m
+
+        if n_voices is None:
+            nq = np.ceil((bw * T * (1 + 2.0 / R) * np.log((1 + R / 2.0) / (1 - R / 2.0))) / 2)
+            nmin = nq - nq % 2
+            ndflt = 2 ** nextpow2(nmin)
+            n_voices = int(ndflt)
+        self.n_voices = n_voices
+
+    def _geometric_sample(self):
+        k = np.arange(1, self.n_voices + 1)
+        self.q = (self.fmax / self.fmin) ** (1 / (self.n_voices - 1.0))
+        t = np.arange(1, self.signal.shape[0] + 1) - self.m - 1
+        geo_f = self.fmin * np.exp((k - 1) * np.log(self.q))
+        self.geo_f = geo_f
+        tfmatx = np.exp(-2 * 1j * np.dot(t.reshape(t.shape[0], 1),
+                                        geo_f.reshape(1, geo_f.shape[0])) * np.pi)
+        S1 = np.dot(self.s1.reshape(1, self.s1.shape[0]), tfmatx)
+        S2 = np.dot(self.s2.reshape(1, self.s2.shape[0]), tfmatx)
+        S1 = np.append(S1, np.zeros((self.n_voices,)))
+        S2 = np.append(S2, np.zeros((self.n_voices,)))
+        return S1, S2
+
+    def _mellin_transform(self, S1, S2):
+        p = np.arange(2 * self.n_voices)
+        mellin1 = np.fft.fftshift(np.fft.ifft(S1))
+        mellin2 = np.fft.fftshift(np.fft.ifft(S2))
+        umin = -self.umax
+        du = np.abs(self.umax - umin) / (2 * self.m1)
+        u = np.linspace(umin, self.umax - du, (self.umax - umin) / du)
+        u[self.m1] = 0
+        self.u = u
+        beta = (p / float(self.n_voices) - 1) / (2 * np.log(self.q))
+        return beta, mellin1, mellin2
+
+    def run(self):
+        S1, S2 = self._geometric_sample()
+        beta, mellin1, mellin2 = self._mellin_transform(S1, S2)
+        # Computation of P0(t. f, f)
+        waf = np.zeros((2 * self.m1, self.n_voices), dtype=complex)
+        for n in np.hstack((np.arange(1, self.m1 + 1), np.arange(self.m1 + 2, 2 * self.m1 + 1))):
+            mx1 = np.exp((-2 * 1j * np.pi * beta + 0.5) * np.log((self.u[n - 1] / 2) *
+                np.exp(-self.u[n - 1] / 2.0) / np.sinh(self.u[n - 1] / 2))) * mellin1
+            mx2 = np.exp((-2 * 1j * np.pi * beta + 0.5) * np.log((self.u[n - 1] / 2) *
+                np.exp(self.u[n - 1] / 2.0) / np.sinh(self.u[n - 1] / 2))) * mellin2
+            fx1 = np.fft.fft(np.fft.fftshift(mx1))[:self.n_voices]
+            fx2 = np.fft.fft(np.fft.fftshift(mx2))[:self.n_voices]
+            waf[n - 1, :] = fx1 * np.conj(fx2)
+        waf[self.m1, :] = S1[:self.n_voices] * np.conj(S2[:self.n_voices])
+        waf = np.vstack((waf[self.m1:(2 * self.m1), :], waf[:self.m1, :]))
+        waf *= np.repeat(self.geo_f.reshape((1, self.geo_f.shape[0])), 2 * self.m1, axis=0)
+        tffr = np.fft.ifft(waf, axis=0)
+        tffr = np.real(np.rot90(np.vstack((tffr[self.m1:(2 * self.m1 + 1), :],
+                                        tffr[:self.m1, :])), k=-1))
+        # conversion from tff to tf using 1d interpolation
+        tfr = np.zeros((self.n_voices, self.ts.shape[0]))
+        ts2 = (self.signal.shape[0] - 1.0) / 2
+        gamma = np.linspace(-self.geo_f[self.n_voices - 1] * ts2,
+                            self.geo_f[self.n_voices - 1] * ts2, 2 * self.m1)
+        for i in range(self.n_voices):
+            ind = find(np.logical_and(gamma >= -self.geo_f[i] * ts2,
+                                    gamma <= self.geo_f[i] * ts2))
+            x = gamma[ind]
+            y = tffr[i, ind]
+            xi = (self.ts - ts2) * self.geo_f[i]
+            tck = splrep(x, y)
+            tfr[i, :] = splev(xi, tck).ravel()
+        t = self.ts
+        f = self.geo_f.ravel()
+        self.freqs = f
+
+        # Normalization
+        SP1 = np.fft.fft(hilbert(self.s1), axis=0)
+        SP2 = np.fft.fft(hilbert(self.s2), axis=0)
+        indmin = 1 + int(np.round(self.fmin * (self.ts.shape[0] - 2)))
+        indmax = 1 + int(np.round(self.fmax * (self.ts.shape[0] - 2)))
+        sp1_ana = SP1[(indmin - 1):indmax]
+        sp2_ana = SP2[(indmin - 1):indmax]
+
+        if self.kind == "auto":
+            multiplier = np.linalg.norm(sp1_ana) ** 2
+        else:
+            multiplier = np.dot(sp1_ana, sp2_ana)
+
+        tfr = tfr * multiplier / integrate_2d(tfr, t, f) / self.n_voices
+        from IPython.core.debugger import Tracer
+        Tracer()()
+        self.tfr = tfr
+        return tfr, t, f
+
+
+def bertrand(signal, timestamps=None, fmin=None, fmax=None, n_voices=None):
+    """bertrand
+
+    :param signal:
+    :param timestamps:
+    :param fmin:
+    :param fmax:
+    :param n_voices:
+    :type signal:
+    :type timestamps:
+    :type fmin:
+    :type fmax:
+    :type n_voices:
+:return:
+:rtype:
+    """
+    xrow = signal.shape[0]
+    if timestamps is None:
+        timestamps = np.arange(xrow)
+
+    tcol = timestamps.shape[0]
+    x1 = signal.copy()
+    x2 = signal.copy()
+
+    s1 = np.real(x1)
+    s2 = np.real(x2)
+    m = (xrow + (xrow % 2)) / 2
+    t = np.arange(xrow) - m - 1
+    tmin = 1
+    tmax = xrow
+    T = tmax - tmin
+    mt = xrow
+
+    if (fmin is None) or (fmax is None):
+        stf1 = np.fft.fft(np.fft.fftshift(s1[timestamps.min():timestamps.max() + 1]))
+        stf2 = np.fft.fft(np.fft.fftshift(s2[timestamps.min():timestamps.max() + 1]))
+        nstf = stf1.shape[0]
+        sp1 = np.abs(stf1[:int(np.round(nstf / 2.0))]) ** 2
+        sp2 = np.abs(stf2[:int(np.round(nstf / 2.0))]) ** 2
+        maxsp1 = sp1.max()
+        maxsp2 = sp2.max()
+        f = np.linspace(0, 0.5, np.round(nstf / 2.0) + 1)
+        if fmin is None:
+            mask = sp1 > maxsp1 / 100.0
+            indmin = np.arange(mask.shape[0], dtype=int)[mask.astype(bool)].min()
+            fmin = max([0.01, 0.05 * np.floor(f[indmin] / 0.05)])
+        if fmax is None:
+            mask = sp2 > maxsp2 / 100.0
+            indmax = np.arange(mask.shape[0], dtype=int)[mask.astype(bool)].max()
+            fmax = 0.05 * np.ceil(f[indmax] / 0.05)
+    bw = fmax - fmin
+    R = bw / (fmin + fmax) * 2.0
+    umaxbert = lambda x: np.exp(x) - fmax / fmin
+    umax = brenth(umaxbert, 0, 4)
+    teq = m / (fmax * umax)
+    if teq < mt:
+        m0 = np.round((2 * m ** 2) / teq - m) + 1
+        m1 = m + m0
+        T = 2 * m1 - 1
+    else:
+        m0 = 0
+        m1 = m
+
+    if n_voices is None:
+        nq = np.ceil((bw * T * (1 + 2.0 / R) * np.log((1 + R / 2.0) / (1 - R / 2.0))) / 2)
+        nmin = nq - nq % 2
+        ndflt = 2 ** nextpow2(nmin)
+        n_voices = int(ndflt)
+
+    # Geometric sampling for the analyzed spectrum
+    k = np.arange(1, n_voices + 1)
+    q = (fmax / fmin) ** (1 / (n_voices - 1.0))
+    t = np.arange(1, mt + 1) - m - 1
+    geo_f = fmin * np.exp((k - 1) * np.log(q))
+    tfmatx = np.exp(-2 * 1j * np.dot(t.reshape(t.shape[0], 1),
+                                     geo_f.reshape(1, geo_f.shape[0])) * np.pi)
+    S1 = np.dot(s1.reshape(1, s1.shape[0]), tfmatx)
+    S2 = np.dot(s2.reshape(1, s2.shape[0]), tfmatx)
+    S1 = np.append(S1, np.zeros((n_voices,)))
+    S2 = np.append(S2, np.zeros((n_voices,)))
+
+    # Mellin tranform of signal
+    p = np.arange(2 * n_voices)
+    mellin1 = np.fft.fftshift(np.fft.ifft(S1))
+    mellin2 = np.fft.fftshift(np.fft.ifft(S2))
+    umin = -umax
+    du = np.abs(umax - umin) / (2 * m1)
+    u = np.linspace(umin, umax - du, (umax - umin) / du)
+    u[m1] = 0
+    beta = (p / float(n_voices) - 1) / (2 * np.log(q))
+
+    # Computation of P0(t. f, f)
+    waf = np.zeros((2 * m1, n_voices), dtype=complex)
+    for n in np.hstack((np.arange(1, m1 + 1), np.arange(m1 + 2, 2 * m1 + 1))):
+        mx1 = np.exp((-2 * 1j * np.pi * beta + 0.5) * np.log((u[n - 1] / 2) *
+            np.exp(-u[n - 1] / 2.0) / np.sinh(u[n - 1] / 2))) * mellin1
+        mx2 = np.exp((-2 * 1j * np.pi * beta + 0.5) * np.log((u[n - 1] / 2) *
+            np.exp(u[n - 1] / 2.0) / np.sinh(u[n - 1] / 2))) * mellin2
+        fx1 = np.fft.fft(np.fft.fftshift(mx1))[:n_voices]
+        fx2 = np.fft.fft(np.fft.fftshift(mx2))[:n_voices]
+        waf[n - 1, :] = fx1 * np.conj(fx2)
+    waf[m1, :] = S1[:n_voices] * np.conj(S2[:n_voices])
+    waf = np.vstack((waf[m1:(2 * m1), :], waf[:m1, :]))
+    waf *= np.repeat(geo_f.reshape((1, geo_f.shape[0])), 2 * m1, axis=0)
+    tffr = np.fft.ifft(waf, axis=0)
+    tffr = np.real(np.rot90(np.vstack((tffr[m1:(2 * m1 + 1), :],
+                                       tffr[:m1, :])), k=-1))
+    # conversion from tff to tf using 1d interpolation
+    tfr = np.zeros((n_voices, tcol))
+    ts2 = (mt - 1.0) / 2
+    gamma = np.linspace(-geo_f[n_voices - 1] * ts2,
+                        geo_f[n_voices - 1] * ts2, 2 * m1)
+    for i in range(n_voices):
+        ind = find(np.logical_and(gamma >= -geo_f[i] * ts2,
+                                  gamma <= geo_f[i] * ts2))
+        x = gamma[ind]
+        y = tffr[i, ind]
+        xi = (timestamps - ts2 - 1) * geo_f[i]
+        tck = splrep(x, y)
+        tfr[i, :] = splev(xi, tck).ravel()
+    t = timestamps
+    f = geo_f.ravel()
+
+    # Normalization
+    SP1 = np.fft.fft(hilbert(s1), axis=0)
+    SP2 = np.fft.fft(hilbert(s2), axis=0)
+    indmin = 1 + int(np.round(fmin * (tcol - 2)))
+    indmax = 1 + int(np.round(fmax * (tcol - 2)))
+    sp1_ana = SP1[(indmin - 1):indmax]
+    sp2_ana = SP2[(indmin - 1):indmax]
+
+    tfr = tfr * np.dot(sp1_ana.T, sp2_ana) / integrate_2d(tfr, t, f) / n_voices
+    return tfr, t, f
 
 
 def lambdak(u, k):
@@ -455,141 +746,6 @@ def d_flandrin(signal, timestamps=None, fmin=None, fmax=None, n_voices=None):
     sp2_ana = SP2[(indmin - 1):indmax]
     tfr = tfr * np.dot(sp1_ana.T, sp2_ana) / integrate_2d(tfr, t, f) / n_voices
 
-    return tfr, t, f
-
-
-def bertrand(signal, timestamps=None, fmin=None, fmax=None, n_voices=None):
-    """bertrand
-
-    :param signal:
-    :param timestamps:
-    :param fmin:
-    :param fmax:
-    :param n_voices:
-    :type signal:
-    :type timestamps:
-    :type fmin:
-    :type fmax:
-    :type n_voices:
-:return:
-:rtype:
-    """
-    xrow = signal.shape[0]
-    if timestamps is None:
-        timestamps = np.arange(xrow)
-
-    tcol = timestamps.shape[0]
-    x1 = signal.copy()
-    x2 = signal.copy()
-
-    s1 = np.real(x1)
-    s2 = np.real(x2)
-    m = (xrow + (xrow % 2)) / 2
-    t = np.arange(xrow) - m - 1
-    tmin = 1
-    tmax = xrow
-    T = tmax - tmin
-    mt = xrow
-
-    if (fmin is None) or (fmax is None):
-        stf1 = np.fft.fft(np.fft.fftshift(s1[timestamps.min():timestamps.max() + 1]))
-        stf2 = np.fft.fft(np.fft.fftshift(s2[timestamps.min():timestamps.max() + 1]))
-        nstf = stf1.shape[0]
-        sp1 = np.abs(stf1[:int(np.round(nstf / 2.0))]) ** 2
-        sp2 = np.abs(stf2[:int(np.round(nstf / 2.0))]) ** 2
-        maxsp1 = sp1.max()
-        maxsp2 = sp2.max()
-        f = np.linspace(0, 0.5, np.round(nstf / 2.0) + 1)
-        if fmin is None:
-            mask = sp1 > maxsp1 / 100.0
-            indmin = np.arange(mask.shape[0], dtype=int)[mask.astype(bool)].min()
-            fmin = max([0.01, 0.05 * np.floor(f[indmin] / 0.05)])
-        if fmax is None:
-            mask = sp2 > maxsp2 / 100.0
-            indmax = np.arange(mask.shape[0], dtype=int)[mask.astype(bool)].max()
-            fmax = 0.05 * np.ceil(f[indmax] / 0.05)
-    bw = fmax - fmin
-    R = bw / (fmin + fmax) * 2.0
-    umaxbert = lambda x: np.exp(x) - fmax / fmin
-    umax = brenth(umaxbert, 0, 4)
-    teq = m / (fmax * umax)
-    if teq < mt:
-        m0 = np.round((2 * m ** 2) / teq - m) + 1
-        m1 = m + m0
-        T = 2 * m1 - 1
-    else:
-        m0 = 0
-        m1 = m
-
-    if n_voices is None:
-        nq = np.ceil((bw * T * (1 + 2.0 / R) * np.log((1 + R / 2.0) / (1 - R / 2.0))) / 2)
-        nmin = nq - nq % 2
-        ndflt = 2 ** nextpow2(nmin)
-        n_voices = int(ndflt)
-
-    # Geometric sampling for the analyzed spectrum
-    k = np.arange(1, n_voices + 1)
-    q = (fmax / fmin) ** (1 / (n_voices - 1.0))
-    t = np.arange(1, mt + 1) - m - 1
-    geo_f = fmin * np.exp((k - 1) * np.log(q))
-    tfmatx = np.exp(-2 * 1j * np.dot(t.reshape(t.shape[0], 1),
-                                     geo_f.reshape(1, geo_f.shape[0])) * np.pi)
-    S1 = np.dot(s1.reshape(1, s1.shape[0]), tfmatx)
-    S2 = np.dot(s2.reshape(1, s2.shape[0]), tfmatx)
-    S1 = np.append(S1, np.zeros((n_voices,)))
-    S2 = np.append(S2, np.zeros((n_voices,)))
-
-    # Mellin tranform of signal
-    p = np.arange(2 * n_voices)
-    mellin1 = np.fft.fftshift(np.fft.ifft(S1))
-    mellin2 = np.fft.fftshift(np.fft.ifft(S2))
-    umin = -umax
-    du = np.abs(umax - umin) / (2 * m1)
-    u = np.linspace(umin, umax - du, (umax - umin) / du)
-    u[m1] = 0
-    beta = (p / float(n_voices) - 1) / (2 * np.log(q))
-
-    # Computation of P0(t. f, f)
-    waf = np.zeros((2 * m1, n_voices), dtype=complex)
-    for n in np.hstack((np.arange(1, m1 + 1), np.arange(m1 + 2, 2 * m1 + 1))):
-        mx1 = np.exp((-2 * 1j * np.pi * beta + 0.5) * np.log((u[n - 1] / 2) *
-            np.exp(-u[n - 1] / 2.0) / np.sinh(u[n - 1] / 2))) * mellin1
-        mx2 = np.exp((-2 * 1j * np.pi * beta + 0.5) * np.log((u[n - 1] / 2) *
-            np.exp(u[n - 1] / 2.0) / np.sinh(u[n - 1] / 2))) * mellin2
-        fx1 = np.fft.fft(np.fft.fftshift(mx1))[:n_voices]
-        fx2 = np.fft.fft(np.fft.fftshift(mx2))[:n_voices]
-        waf[n - 1, :] = fx1 * np.conj(fx2)
-    waf[m1, :] = S1[:n_voices] * np.conj(S2[:n_voices])
-    waf = np.vstack((waf[m1:(2 * m1), :], waf[:m1, :]))
-    waf *= np.repeat(geo_f.reshape((1, geo_f.shape[0])), 2 * m1, axis=0)
-    tffr = np.fft.ifft(waf, axis=0)
-    tffr = np.real(np.rot90(np.vstack((tffr[m1:(2 * m1 + 1), :],
-                                       tffr[:m1, :])), k=-1))
-    # conversion from tff to tf using 1d interpolation
-    tfr = np.zeros((n_voices, tcol))
-    ts2 = (mt - 1.0) / 2
-    gamma = np.linspace(-geo_f[n_voices - 1] * ts2,
-                        geo_f[n_voices - 1] * ts2, 2 * m1)
-    for i in range(n_voices):
-        ind = find(np.logical_and(gamma >= -geo_f[i] * ts2,
-                                  gamma <= geo_f[i] * ts2))
-        x = gamma[ind]
-        y = tffr[i, ind]
-        xi = (timestamps - ts2 - 1) * geo_f[i]
-        tck = splrep(x, y)
-        tfr[i, :] = splev(xi, tck).ravel()
-    t = timestamps
-    f = geo_f.ravel()
-
-    # Normalization
-    SP1 = np.fft.fft(hilbert(s1), axis=0)
-    SP2 = np.fft.fft(hilbert(s2), axis=0)
-    indmin = 1 + int(np.round(fmin * (tcol - 2)))
-    indmax = 1 + int(np.round(fmax * (tcol - 2)))
-    sp1_ana = SP1[(indmin - 1):indmax]
-    sp2_ana = SP2[(indmin - 1):indmax]
-
-    tfr = tfr * np.dot(sp1_ana.T, sp2_ana) / integrate_2d(tfr, t, f) / n_voices
     return tfr, t, f
 
 