97e8534f62a68a204f95a6d28c16a64ee3d1c291
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2016-02-18 17:10:18 +0530

    Fix constant frequency modulation

--- tftb/generators/frequency_modulated.py
+++ tftb/generators/frequency_modulated.py
@@ -29,9 +29,9 @@ def fmconst(n_points, fnorm=0.25, t0=None):
     elif abs(fnorm) > 0.5:
         raise TypeError
     else:
-        tmt0 = np.arange(1, n_points + 1) - t0
+        tmt0 = np.arange(n_points) - t0
         y = np.exp(1j * 2.0 * np.pi * fnorm * tmt0)
-        y = y / np.real(np.int(np.real(y[t0 - 1])))
+        y = y / np.real(y[int(t0)])
         iflaw = fnorm * np.ones((n_points,))
         return y, iflaw
 
