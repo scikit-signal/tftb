415c960ddec97813498abe34c670d4f539f14ad2
ccakshay5 <akshay14133@iiitd.ac.in>
2016-03-05 14:42:23 +0530

    changed 2 files

--- tftb/processing/postprocessing.py
+++ tftb/processing/postprocessing.py
@@ -32,39 +32,12 @@ def hough_transform(image, m=None, n=None):
     if n is None:
         n = ymax
 
-    rhomax = np.sqrt((xmax ** 2) + (ymax ** 2)) / 2.0
-    deltar = rhomax / (m - 1.0)
-    deltat = 2 * np.pi / n
+    image  =  image[0:m , 0:n]
 
-    ht = np.zeros((m, n))
-    imax = np.amax(image)
+    ht, theta, rho = hough_line(image)
 
-    if xmax % 2 != 0:
-        xc = (xmax + 1) / 2
-        xf = xc - 1
-    else:
-        xc = xf = xmax / 2
-    x0 = 1 - xc
-
-    if ymax % 2 != 0:
-        yc = (ymax + 1) / 2
-        yf = yc - 1
-    else:
-        yc = yf = ymax / 2
-    y0 = 1 - yc
-
-    for x in range(x0, xf + 1):
-        for y in range(y0, yf + 1):
-            if np.abs(image[x + xc - 1, y + yc - 1]) > imax / 20.0:
-                for theta in np.linspace(0, 2 * np.pi - deltat, n):
-                    rho = x * np.cos(theta) - y * np.sin(theta)
-                    if (rho >= 0) and (rho <= rhomax):
-                        ht[int(np.round(rho / deltar)),
-                           int(np.round(theta / deltat))] += image[x + xc - 1,
-                                                                   y + yc - 1]
-    rho = np.linspace(0, rhomax, n)
-    theta = np.linspace(0, 2 * np.pi - deltat, n)
     return ht, rho, theta
+   
 
 
 def renyi_information(tfr, timestamps=None, freq=None, alpha=3.0):
